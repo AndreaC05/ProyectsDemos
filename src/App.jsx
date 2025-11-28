1c6dec022de65aa83819ebc716ed1c68dbedd3b2
Andrea Aguirre <aguirreandrea457@gmail.com>
2025-11-27 21:10:44 -0500

    avabnce

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-import "primeicons/primeicons.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 //IMPORTACIONES DE PAGINAS 
